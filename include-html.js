5e865a7c955f083b716a2d7b0f23bc065a8aa777
thatvisualguy <124064502+thatvisualguy@users.noreply.github.com>
2023-12-27 13:36:53 +0530

    Delete include-html.js

--- include-html.js
+++
@@ -1,9 +0,0 @@
-function includeHTML(file, containerID){
-fetch(file)
-.then(response => response.text())
-.then(data => document.getElementById(containerID).innerHTML = data);
-}
-
-document.addEventListener('DOMContentLoaded', function(){
-includeHTML('C:/Users/20034124/Desktop/www/Assets/Common/header_navbar.html', 'header-container');
-});
