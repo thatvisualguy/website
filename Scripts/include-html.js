149e80f23d7b7b9f468424a8429c0283fdf7ead3
thatvisualguy <124064502+thatvisualguy@users.noreply.github.com>
2023-12-28 16:12:23 +0530

    Update include-html.js

--- Scripts/include-html.js
+++ Scripts/include-html.js
@@ -1,9 +1,34 @@
-function includeHTML(file, containerID){
-fetch(file)
-.then(response => response.text())
-.then(data => document.getElementById(containerID).innerHTML = data);
+function includeHTML(file, containerID, callback) {
+  fetch(file)
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById(containerID).innerHTML = data;
+      if (callback && typeof callback === 'function') {
+        callback();
+      }
+    });
 }
 
-document.addEventListener('DOMContentLoaded', function(){
-includeHTML('Assets/Common/header_navbar.html', 'header-container');
+document.addEventListener('DOMContentLoaded', function() {
+  includeHTML('https://raw.githubusercontent.com/thatvisualguy/website/main/Assets/Common/header_navbar.html', 'header-container', () => {
+    // This is the callback function
+    const hamburgerMenu = document.querySelector('.hamburger-menu');
+    const sidenav = document.querySelector('.sidenav');
+    const element = document.getElementById('categories');
+    const element2 = document.getElementById('categories2');
+    const element3 = document.getElementById('categories3');
+
+    if (hamburgerMenu) {
+      hamburgerMenu.addEventListener('click', () => {
+        sidenav.classList.toggle('active');
+        element.classList.toggle('active');
+        element2.classList.toggle('active');
+        element3.classList.toggle('active');
+      });
+    }
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  includeHTML('https://raw.githubusercontent.com/thatvisualguy/website/main/Assets/Common/footer.html', 'footer-container');
 });
