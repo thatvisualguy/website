4616752c9d24182881a2dee5961109f3792a783b
thatvisualguy <124064502+thatvisualguy@users.noreply.github.com>
2023-12-29 10:19:34 +0530

    Update header_navbar.js

--- Assets/Common/header_navbar.js
+++ Assets/Common/header_navbar.js
@@ -1,15 +1,21 @@
 // Toggle navigation bar
-const hamburgerMenu = document.querySelector('.hamburger-menu');
-const sidenav = document.querySelector('.sidenav');
-const element = document.getElementById("categories");
-const element2 = document.getElementById("categories2");
-const element3 = document.getElementById("categories3");
+document.addEventListener("DOMContentLoaded", () => {
+  includeHTML('https://raw.githubusercontent.com/thatvisualguy/website/main/Assets/Common/header_navbar.html', () => {
+    const hamburgerMenu = document.querySelector(".hamburger-menu");
+    const sidenav = document.querySelector(".sidenav");
+    const element = document.getElementById("categories");
+    const element2 = document.getElementById("categories2");
+    const element3 = document.getElementById("categories3");
 
-hamburgerMenu.addEventListener('click', () => {
-    sidenav.classList.toggle('active');
-  element.classList.toggle('active');
-  element2.classList.toggle('active');
-  element3.classList.toggle('active');
+    if (hamburgerMenu) {
+      hamburgerMenu.addEventListener("click", () => {
+        sidenav.classList.toggle("active");
+        element.classList.toggle("active");
+        element2.classList.toggle("active");
+        element3.classList.toggle("active");
+      });
+    }
+  });
 });
 
 
@@ -18,8 +24,7 @@ function myAccFunc() {
   var x = document.getElementById("demoAcc");
   if (x.className.indexOf("w3-show") == -1) {
     x.className += " w3-show";
-
-  } else { 
+  } else {
     x.className = x.className.replace(" w3-show", "");
   }
 }
@@ -28,8 +33,16 @@ function myAccFunc1() {
   var x = document.getElementById("demoAcc1");
   if (x.className.indexOf("w3-show") == -1) {
     x.className += " w3-show";
+  } else {
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
 
-  } else { 
+function myAccFunc2() {
+  var x = document.getElementById("demoAcc2");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else {
     x.className = x.className.replace(" w3-show", "");
   }
 }
