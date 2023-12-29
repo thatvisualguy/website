236e9fd2a51cf1973e673534d440ca22786592af
thatvisualguy <124064502+thatvisualguy@users.noreply.github.com>
2023-12-29 10:26:50 +0530

    Create index.js

---
+++ Scripts/index.js
@@ -0,0 +1,33 @@
+//Mockup Crousel
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "inline";
+  x[myIndex-1].style.animation = "slide-left 1s forwards";
+  setTimeout(carousel, 3000); // Change image every 2 seconds
+}
+
+//FAQs
+const items = document.querySelectorAll(".accordion button");
+
+function toggleAccordion() {
+  const itemToggle = this.getAttribute('aria-expanded');
+  
+  for (i = 0; i < items.length; i++) {
+    items[i].setAttribute('aria-expanded', 'false');
+  }
+  
+  if (itemToggle == 'false') {
+    this.setAttribute('aria-expanded', 'true');
+  }
+}
+
+items.forEach(item => item.addEventListener('click', toggleAccordion));
